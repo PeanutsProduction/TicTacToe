8f1164809a792183cb71d5d6a856e6b796790dd1
PeanutsProduction <93841284+PeanutsProduction@users.noreply.github.com>
2024-01-13 20:53:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Board:
+
+    def __init__(self,x,y):
+         self.sizeX = x
+         self.sizeY = y
+         self.board = [["1" for i in range(self.sizeX)] for i in range(self.sizeY)]
+
+    
+    def __str__(self):
+        for i in range(self.sizeY):
+            line = ""
+            for j in range(self.sizeX):
+                line += self.board[i][j] + " "
+            print(line)
+
+
+test = Board(3,3)
+print(test)
+
+            
+    
