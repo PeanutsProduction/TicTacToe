1079fd4ae513b60dc63fb0f6a355c6b8b98eca07
PeanutsProduction <93841284+PeanutsProduction@users.noreply.github.com>
2024-01-14 14:23:38 +0000

    updates and added ui library (tkinter)

--- board.py
+++ board.py
@@ -1,22 +1,31 @@
 class Board:
 
-    def __init__(self,x):
-         self.size = x
-         self.board = ['E' for i in range(self.size**2)]
+    def __init__(self,givenSize):
+        if givenSize <= 0:
+            print("ERROR! Board size must be 3 or bigger \nSetting board size to 3 ... \n")
+            givenSize = 3
+        self.size = givenSize
+        self.board = ['E' for i in range(self.size**2)]
+        # dictonary coordinates test
+        self.altBoard = {}
+        for i in range(self.size):
+            for j in range(self.size):
+                self.altBoard[(i,j)] = "#"
 
     
     def __str__(self): # [1,2,3,1,2,3,1,2,3]
         line = ""
         counter = 1
-        for i in self.board:
+        for i in self.altBoard:
             if counter == self.size:
-                line += i + "\n"
+                line += self.altBoard[i] + "\n"
                 counter = 0
             else:
-                line += i + "|"
+                line += self.altBoard[i] + "|"
             counter += 1
         print(line)
-        return "BOARD LOADED"
+        return ""
+
 
             
     
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from board import Board
+import tkinter as tk
+from tk import *
 
 test = Board(3)
 print(test)
+
+ui = tk.Tk()
+ui.geometry("500x500")
+ui.mainloop()
